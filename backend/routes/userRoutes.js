a73f85145893f48647d8c8a04f21c06a83e0bb10
bogdanmatei <matei90bogdan@yahoo.com>
2020-10-19 15:04:50 +0300

    Admin middleware and get users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,12 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
-import protect from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
+router.route('/').get(protect, admin, getUsers);
 router.route('/').post(registerUser);
 router.post('/login', authUser);
 router
