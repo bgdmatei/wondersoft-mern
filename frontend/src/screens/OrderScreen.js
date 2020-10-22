adf3d49c30bb5202f4d01fd34dac5445593058d9
bogdanmatei <matei90bogdan@yahoo.com>
2020-10-22 15:48:33 +0300

    Frontend product reviews

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -72,7 +72,7 @@ const OrderScreen = ({ match, history }) => {
         setSdkReady(true);
       }
     }
-  }, [dispatch, order, orderId, successPay, successDeliver, history]);
+  }, [dispatch, order, orderId, successPay, successDeliver, history, userInfo]);
 
   const successPaymentHandler = (paymentResult) => {
     dispatch(payOrder(orderId, paymentResult));
